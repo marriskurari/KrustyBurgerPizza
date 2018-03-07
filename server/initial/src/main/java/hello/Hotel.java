287054c0819d335acca240a47ffdc8dd283874d5
olafur-palsson <olafur.palsson2@gmail.com>
2018-03-07 14:05:02 +0000

    Lagaði smá uppsetninguna á kóðanum á 'hotel' svo hann lúkki og taki minna pláss

--- server/initial/src/main/java/hello/Hotel.java
+++ server/initial/src/main/java/hello/Hotel.java
@@ -14,63 +14,31 @@ public class Hotel {
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Integer id;
 	private Integer numRooms;
-	private double latitude;
-	private Double longtitude;		
-	private String name;
-	private String email;
+	private double  latitude;
+	private Double  longtitude;		
+	private String  name;
+	private String  email;
+
 	private ArrayList<String> amenities;
-  //private Date[] closedDates; 
+	//private Date[] closedDates; 
 
-	public Integer getId() {
-		return id;
-	}
+	public Integer getId()					 { return id;		 } 
+	public void    setId(Integer id) { this.id = id; }
+   
+	public Integer getNumRooms()								 { return numRooms;				   } 
+	public void    setNumRooms(Integer numRooms) { this.numRooms = numRooms; }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+	public double getLatitude()								 { return latitude;				   } 
+	public void   setLatitude(double latitude) { this.latitude = latitude; }
 
-	public Integer getNumRooms() {
-		return numRooms;
-	}
+	public Double getLongtitude()									 { return longtitude;					   } 
+	public void   setLongtitude(Double longtitude) { this.longtitude = longtitude; }
 
-	public void setNumRooms(Integer numRooms) {
-		System.out.println("numrooms set to " + numRooms);
-		this.numRooms = numRooms;
-	}
+	public String getName()						 { return name;			 } 
+	public void   setName(String name) { this.name = name; }
 
-	public double getLatitude() {
-		return latitude;
-	}
+	public String getEmail()						 { return email;			 } 
+	public void   setEmail(String email) { this.email = email; }
 
-	public void setLatitude(double latitude) {
-		this.latitude = latitude;
-	}
-	
-	public Double getLongtitude() {
-		return longtitude;
-	}
-
-	public void setLongtitude(Double longtitude) {
-		this.longtitude = longtitude;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setAmenities(ArrayList<String> amenities) {
-		this.amenities = amenities;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
+	public void setAmenities(ArrayList<String> amenities) { this.amenities = amenities; }
 }
